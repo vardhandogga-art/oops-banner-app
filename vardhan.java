89400d31d94921504874ca4b690625a72e6c6f8c
vardhandogga-art <honeydogga2013@gmail.com>
2026-02-17 09:41:49 +0530

    Update vardhan.java

--- vardhan.java
+++ vardhan.java
@@ -1,7 +1,27 @@
-public class vardhan {
+public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS!");
+
+        System.out.println(String.join(" ",
+                "******", "  ****  ", " ****** ", "******"));
+
+        System.out.println(String.join(" ",
+                "*      *", "*    *", "*      *", "*"));
+
+        System.out.println(String.join(" ",
+                "*      *", "*    *", "*      *", "*"));
+
+        System.out.println(String.join(" ",
+                "*      *", "*    *", "*      *", "******"));
+
+        System.out.println(String.join(" ",
+                "*      *", "*    *", "*      *", "     *"));
+
+        System.out.println(String.join(" ",
+                "*      *", "*    *", "*      *", "*    *"));
+
+        System.out.println(String.join(" ",
+                "******", "  ****  ", " ****** ", " **** "));
     }
 }
-    
 
